81777e1b12acd51dde191b7c7a675e9e9b77c1e0
Douglas Rodrigues <123272282+Zatanaris@users.noreply.github.com>
2023-05-31 11:54:49 -0300

    Update ValidaCpf.js

--- ValidaCpf.js
+++ ValidaCpf.js
@@ -44,10 +44,3 @@ class ValidaCpf {
    }
 }
 
-/* const validaCpf = new ValidaCpf("070.987.720-03");
-
-if (validaCpf.Valida()) {
-   console.log("CPF valido.");
-} else {
-   console.log("CPF invalido, Digite um cpf valido.");
-} */
